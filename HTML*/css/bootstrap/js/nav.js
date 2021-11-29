0209fdfce24b8cca9d0a857705ccbb95946b4fa3
Yatri Shah <84889461+Yatrishah@users.noreply.github.com>
2021-11-29 10:44:44 +0530

    Add files via upload

---
+++ HTML*/css/bootstrap/js/nav.js
@@ -0,0 +1,9 @@
+const toggleButton=document.getElementsByClassName('toggle-Button	')[0]
+const navbarLinks=document.getElementsByClassName	('navbar-links')[0]
+
+toggleButton.addEventListener('click',()=>{
+
+navbarLinks.classList.toggle('active')
+
+
+})
