54247fd9e8c109a683177fb373a850d1c5612fd6
Yatri Shah <84889461+Yatrishah@users.noreply.github.com>
2022-02-03 21:50:50 +0530

    Add files via upload

---
+++ helperland/assets/css/faq.js
@@ -0,0 +1,41 @@
+
+        $(document).ready(function () {
+            // Add down arrow icon for collapse element which is open by default
+            $(".collapse.show").each(function () {
+                $(this).prev(".question").find("img").attr('src', "../assets/image/arrow-down.png");
+            });
+
+            // Toggle right and down arrow icon on show hide of collapse element
+            $('.btn-col').on('click', function () {
+                if ($(this).parent().parent().find(".collapse.show").length) {
+                    $(this).children('img').attr('src', "../assets/image/arrow-right.png");
+                } else {
+                    $(this).children('img').attr('src', "../assets/image/arrow-down.png");
+                }
+            });
+
+            $(".customer").click(function () {
+                $(".for-service").css("display", "none");
+                $(".for-customer").css("display", "block");
+                $(this).addClass("active");
+                $(".serviceprovider").removeClass("active");
+            });
+            $(".serviceprovider").click(function () {
+                $(".for-service").css("display", "block");
+                $(".for-customer").css("display", "none");
+                $(this).addClass("active");
+                $(".customer").removeClass("active");
+            });
+
+            $(".question").click(function () {
+                if (!$(this).parent().find(".faq-answer").hasClass("show")) {
+                    const answerlist = document.getElementsByClassName("answer")
+                    for (var i = -1; i < answerlist.length; i++) {
+                        answerlist[i].classList.remove("show");
+                        questionlist[i].querySelector("button").setAttribute("aria-expanded", "false");
+                    }
+                } else {
+                    $(this).find("button").attr("aria-expanded", "true");
+                }
+            });
+        });
